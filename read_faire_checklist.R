818bc4fd8078b2db1a0b38bb29e059b92f0250fc
miwa582 <37465264+miwa582@users.noreply.github.com>
2026-09-24 15:05:44 +0800

    Update read_faire_checklist.R
    additing descriptions

--- read_faire_checklist.R
+++ read_faire_checklist.R
@@ -1,8 +1,11 @@
 read_faire_checklist <- function(FAIRe_checklist_ver = 'v1.0.2'){
   if (!requireNamespace("readxl", quietly = TRUE)) install.packages("readxl")
   library(readxl)
+  # Download the checklist (.xlsx) in the working directory 
   source("https://raw.githubusercontent.com/FAIR-eDNA/FAIRe-r/main/download_faire_checklist.R")
   download_faire_checklist(FAIRe_checklist_ver = FAIRe_checklist_ver)
+  # Read the Excel checklist file
   checklist <- as.data.frame(readxl::read_excel(paste0("FAIRe_checklist_", FAIRe_checklist_ver, ".xlsx"), sheet = "checklist"))
+  # Assign
   assign("checklist", checklist, envir = .GlobalEnv)
 }
